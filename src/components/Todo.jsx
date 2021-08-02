602270de0bfa9948e3998d9eab2c53e9c3108532
jaime linares <jmovies1771@gmail.com>
2021-08-02 10:40:27 -0500

    Add localStorage and edit feature

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
+import React from "react";
 
+export const Todo = ({ todo, stateComplete, deleteTodo, editTodo }) => {
+  const [isEdit, setIsEdit] = React.useState(false);
+  const [text, setText] = React.useState("");
 
-export const Todo = ({todo, stateComplete, deleteTodo}) => {
+  function handleCheck() {
+    stateComplete(todo.id);
+  }
 
-    function handleCheck(){
-        stateComplete(todo.id)
+  function handleDelete() {
+    deleteTodo(todo.id);
+  }
 
-    }
-    
-    function handleDelete(){
-        deleteTodo(todo.id)
-    }
+  function hadnleEdit() {
+    setIsEdit(!isEdit);
+    setText(todo.task)
+  }
 
+  function handleSave() {
+    editTodo(todo.id, text);
+    setIsEdit(!isEdit)
+  }
 
+  function handleOnChange(e){
+    setText(e.target.value)
 
-    return (
-        <div style={{display:"flex"}}>
-            <input type="checkbox" onClick={handleCheck}/>
+  }
+  return (
+    <div style={{ display: "flex" }}>
+      <input type="checkbox" onClick={handleCheck} />
 
-
-            
-            <div style={{color:"white", textDecoration: todo.complete ? "line-through": "" }} >
-                
-                {todo.task}
-            </div>
-            <button onClick={handleDelete}>Eliminar</button>
-            
-        </div>
-    )
-}
+      <div
+        style={{
+          color: "white",
+          textDecoration: todo.complete ? "line-through" : "",
+        }}
+      >
+        {isEdit ? <input value={text} onChange={handleOnChange} placeholder={todo.task}></input> : todo.task}
+      </div>
+      {isEdit ? (
+        <button onClick={handleSave}>Guardar</button>
+      ) : (
+        <button onClick={handleDelete}>Eliminar</button>
+      )}
+      {isEdit?<button onClick={hadnleEdit}>Cancelar</button>:<button onClick={hadnleEdit}>Editar</button>}
+    </div>
+  );
+};
